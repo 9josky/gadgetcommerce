694198085f6b70e52ea74af2844e2ed8605dbc59
Oyeniji Sadeeq <nijoskysadeeq@gmail.com>
2023-07-21 12:28:22 +0100

    updated

--- src/components/Template/Footer/Footer.jsx
+++ src/components/Template/Footer/Footer.jsx
@@ -3,7 +3,13 @@ import React from "react";
 function Footer() {
   return (
     <div
-      style={{ display: "flex", justifyContent: "center", marginTop: "50px" }}
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        marginTop: "50px",
+        backgroundColor: "#2a2a72",
+        color: "white",
+      }}
     >
       Created by Oyeniji Sadeeq 2023
     </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { Provider } from './context';
@@ -9,9 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
     </Provider>
     
   </React.StrictMode>
